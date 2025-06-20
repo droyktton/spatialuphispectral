5328d899d73a8271d1d7ebe0363c36c850f15f2d
koltona <droyktton@gmail.com>
2025-06-20 19:17:41 -0300

    program update

--- linearized.cu
+++ linearized.cu
@@ -2,15 +2,23 @@
 #include <thrust/host_vector.h>
 #include <cmath>
 #include <cstdio>
+#include <iostream>
+#include <cuda_runtime.h>
+#include <thrust/copy.h>
+void checkCudaError(cudaError_t err, const char* msg) {
+    if (err != cudaSuccess) {
+        std::cerr << "CUDA error: " << msg << " - " << cudaGetErrorString(err) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
 
 const double alpha = 0.27;
 const double c = 1.0;
-__device__ double H;
 double h_H = 1.001;
 const double phi_start = 0.0;
 const double phi_end = M_PI;
 
-const double h = 0.00001;
+const double h = 0.001;
 const int steps = (phi_end - phi_start) / h;
 
 //const int steps = 100000;
@@ -78,26 +86,34 @@ __device__ void eigenvalues_magnitudes_2x2(double a, double b, double c, double 
 
 
 
-__global__ void solve_all_k(const double* __restrict__ k_vals, double* Q_out, double* Phi_out, int N, double H) {
+__global__ void solve_all_grid(
+    const double* __restrict__ k_vals, const double* __restrict__ H_vals, 
+    double* Q_out, double* Phi_out, int Nk, int Nh) 
+{
+    
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    
     double Q_ini,Phi_ini;
-    if (idx < N) {
+    if (idx < Nk && idy < Nh) 
+    {
         double k = k_vals[idx];
-        //double Q, Phi;
+        double H = H_vals[idy];
 
-	double a, b, c, d;
-
-	Q_ini = 1.0; Phi_ini = 0.0;
-        rk4_solver(k, a, b, Q_ini, Phi_ini, H);
-	
-	Q_ini = 0.0; Phi_ini = 1.0;
-        rk4_solver(k, c, d, Q_ini, Phi_ini, H);
-
-	double lambda1, lambda2;
-	eigenvalues_magnitudes_2x2(a, b, c, d, &lambda1, &lambda2);
-
-        Q_out[idx] = lambda1;
-        Phi_out[idx] = lambda2;
+    	double a, b, c, d;
+    
+    	Q_ini = 1.0; Phi_ini = 0.0;
+            rk4_solver(k, a, b, Q_ini, Phi_ini, H);
+    	
+    	Q_ini = 0.0; Phi_ini = 1.0;
+            rk4_solver(k, c, d, Q_ini, Phi_ini, H);
+    
+    	double lambda1, lambda2;
+    	eigenvalues_magnitudes_2x2(a, b, c, d, &lambda1, &lambda2);
+    
+        Q_out[idx + Nk*idy] = lambda1;
+        Phi_out[idx + Nk*idy] = lambda2;
+        //printf("Thread (%d, %d) processing k=%f, H=%f, (%f, %f)\n", idx, idy, k_vals[idx], H_vals[idy], lambda1, lambda2);
     }
 }
 
@@ -114,50 +130,56 @@ int main(int argc, char **argv) {
     if(argc>5) kend = atof(argv[5]);
     if(argc>6) Nk = atoi(argv[6]);
     	
-    //cudaMemcpyToSymbol(H, &h_H, sizeof(double));
-    //std::cout << "H = " << h_H << std::endl;
-
-    //const int N = 1024;
     thrust::host_vector<double> h_k_vals(Nk);
-    double kmin = kstart, kmax = kend;
+    thrust::host_vector<double> h_H_vals(Nh);
 
     for (int i = 0; i < Nk; ++i)
-        h_k_vals[i] = kmin + i * (kmax - kmin) / (Nk - 1);
+        h_k_vals[i] = kstart + i * (kend - kstart) / (Nk - 1);
+
+    for (int i = 0; i < Nh; ++i)
+        h_H_vals[i] = hstart + i * (hend - hstart) / (Nh - 1);
 
     thrust::device_vector<double> d_k_vals = h_k_vals;
-    thrust::device_vector<double> d_Q_out(Nk), d_Phi_out(Nk);
+    thrust::device_vector<double> d_H_vals = h_H_vals;
+    thrust::device_vector<double> d_Q_out(Nk*Nh), d_Phi_out(Nk*Nh);
 
     //assert(h<0.001);
 
-    int blockSize = 128;
-    int numBlocks = (Nk + blockSize - 1) / blockSize;
+    dim3 blockSize = dim3(16,16);
+    int numBlocks_k = (Nk + blockSize.x - 1) / blockSize.x;
+    int numBlocks_H = (Nh + blockSize.y - 1) / blockSize.y;
+    dim3 numBlocks = dim3(numBlocks_k, numBlocks_H);
 
     FILE* f = fopen("rk4_k_sweep.txt", "w");
     //fprintf(f, "# k H  |lambda1| |lambda2|\n");
 
-    thrust::host_vector<double> h_Q_out(Nk);
-    thrust::host_vector<double> h_Phi_out(Nk);
+    thrust::host_vector<double> h_Q_out(Nk*Nh);
+    thrust::host_vector<double> h_Phi_out(Nk*Nh);
 
-    // loop over periods
-    for(int j=0 ; j<Nh ; ++j){
-      
-      h_H = hstart + j * (hend-hstart) / (Nh-1);		
-      //cudaMemcpyToSymbol(H, &h_H, sizeof(double));
-
-      solve_all_k<<<numBlocks, blockSize>>>(thrust::raw_pointer_cast(d_k_vals.data()),
+    std::cout << "Nk: " << Nk << ", Nh: " << Nh << std::endl;
+    std::cout << "k range: [" << kstart << ", " << kend << "], H range: [" << hstart << ", " << hend << "]" << std::endl;
+    std::cout << "Block size: " << blockSize.x << "x" << blockSize.y << ", Number of blocks: " << numBlocks.x << "x" << numBlocks.y << std::endl;
+    printf("Starting computation...\n");
+    
+    // Launch the kernel
+    solve_all_grid<<<numBlocks, blockSize>>>(
+                                            thrust::raw_pointer_cast(d_k_vals.data()),
+                                            thrust::raw_pointer_cast(d_H_vals.data()),
                                             thrust::raw_pointer_cast(d_Q_out.data()),
-                                            thrust::raw_pointer_cast(d_Phi_out.data()), Nk, h_H);
+                                            thrust::raw_pointer_cast(d_Phi_out.data()), Nk, Nh
+                                        );
+    checkCudaError(cudaGetLastError(), "Kernel launch failed");
 
-      //cudaDeviceSynchronize();
+    thrust::copy(d_Q_out.begin(),d_Q_out.end(), h_Q_out.begin());
+    thrust::copy(d_Phi_out.begin(),d_Phi_out.end(), h_Phi_out.begin());
 
-      thrust::copy(d_Q_out.begin(),d_Q_out.end(), h_Q_out.begin());
-      thrust::copy(d_Phi_out.begin(),d_Phi_out.end(), h_Phi_out.begin());
-
-      for (int i = 0; i < Nk; ++i)
-          fprintf(f, "%.6f %.6f %.6f %.6f\n", h_k_vals[i], h_H, h_Q_out[i], h_Phi_out[i]);
-      fprintf(f, "\n");
-    }
-
+    for (int j = 0; j < Nh; ++j){
+        for (int i = 0; i < Nk; ++i) {
+            fprintf(f, "%lf %lf %lf %lf\n", h_k_vals[i], h_H_vals[j], h_Q_out[i + Nk * j], h_Phi_out[i + Nk * j]);
+        }
+        fprintf(f, "\n");
+    }        
+ 
     fclose(f);
     printf("Results saved to rk4_k_sweep.txt\n");
     return 0;
