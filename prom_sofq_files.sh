1d343430bca48e79e11a5e5f2cb750f8d3281d3b
AleK <droyktton@gmail.com>
2025-06-26 09:06:26 -0300

    program update

--- prom_sofq_files.sh
+++ prom_sofq_files.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
 list=$1
+nf=$2
 
-nf=$(head -n 1 $list | awk '{print NF}')
+#nf=$(head -n 1 $list | awk '{print NF}')
 
 samples=$(ls $list | wc -l)
 
-echo "samples="$samples
-
-echo $list
+#echo $list 
+#echo "nf="$nf" samples="$samples
 
 paste $list > zzz
 
 gawk -v nf="$nf" '{
-	for(j=1;j<=nf;j++)
+	for(j=0;j<nf;j++)
 	{
 		acum[j]=0.0;
+		acum2[j]=0.0;
 		count[j]=0;
 	}; 
 	for(i=1;i<=NF;i++)
 	{
-		acum[(i-1)%nf+1]+=$i;
-		count[(i-1)%nf+1]++;
+		acum[(i-1)%nf]+=$i;
+		acum2[(i-1)%nf]+=$i*$i;
+		count[(i-1)%nf]++;
 	}; 
 	if(NF>0)
 	{
 		for(j=1;j<=nf;j++){
-			printf("%1.2f ",acum[j]/count[j]);
+			media=acum[j-1]/count[j-1];
+			printf("%1.2f ",media);
+		}
+		for(j=1;j<=nf;j++){
+			media=acum[j-1]/count[j-1];
+			var=acum2[j-1]/count[j-1]-media*media;			
+			printf("%1.2f ",var);
 		}
 		printf("\n");
 	} 
---
+++ promediar_roughness.sh
@@ -0,0 +1,2 @@
+L=$1
+bash prom_sofq_files.sh "run_L"$L"_seed12*/z_vs_t.dat" 9 > yyy$L
